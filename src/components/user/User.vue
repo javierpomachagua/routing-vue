a508473ffb545deb5871f8059852e5cb02e5e42c
Javier Pomachagua <javierpp0303@gmail.com>
2019-02-18 18:07:36 -0500

    avance

---
+++ src/components/user/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr>
+    <p>Loaded ID: {{ id }}</p>
+    <button @click="navigateToHome()">Go to Home</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "User",
+  data() {
+    return {
+        id: this.$route.params.id
+    };
+  },
+  methods: {
+    navigateToHome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>
